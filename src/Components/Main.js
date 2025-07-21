95a92238ddc3f38c77a50eaa18b99ef27d369c76
Rahul Paswan <rahulpaswan4626@gmail.com>
2025-07-21 22:27:16 +0530

    Fix some errors!

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,13 +5,13 @@ const Main = () => {
        
         {/* Left Image */}
         <div className="bg-gray-200 rounded-lg flex justify-center items-center">
-          <img src="./assets/imageM1.png" alt="Look 1" className="object-cover" />
+          <img src="../assets/imageM1.png" alt="Look 1" className="object-cover" />
         </div>
 
         {/* Center Content */}
         <div className="text-center flex items-center">
           <div>
-            <img src="./assets/imageM2.png" alt="Group" className="w-full rounded-xl mb-4" />
+            <img src="../assets/imageM2.png" alt="Group" className="w-full rounded-xl mb-4" />
           <h2 className="text-4xl md:text-5xl text-black font-bold">ULTIMATE</h2>
           <h3 className="text-[5rem] font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-400">
             SALE
@@ -21,14 +21,14 @@ const Main = () => {
             Shop Now
           </button>
           <div className="mt-8">
-            <img src="./assets/imageM3.png" alt="Models" className="rounded-lg mx-auto" />
+            <img src="../assets/imageM3.png" alt="Models" className="rounded-lg mx-auto" />
           </div>
           </div>
         </div>
 
         {/* Right Image */}
         <div className="bg-gray-200 rounded-lg flex justify-center items-center">
-          <img src="./assets/imageM4.png" alt="Look 2" className="object-cover" />
+          <img src="../assets/imageM4.png" alt="Look 2" className="object-cover" />
         </div>
       </section>
     </div>
